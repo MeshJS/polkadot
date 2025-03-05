a7485ace013796eb8d19a041a698b442686867c9
Jingles <jinglescode@gmail.com>
2025-03-05 16:51:15 +0800

    init mono repo

---
+++ apps/docs/src/configs/index.ts
@@ -0,0 +1,5 @@
+export const ROOT_URL = "https://polkadot.meshjs.dev";
+
+export const APP_NAME = "polkadot.meshjs.dev";
+export const APP_DESC = "polkadot.meshjs.dev";
+export const PROJECT_GITHUB_URL = "https://github.com/MeshJS/polkadot";
---
+++ apps/docs/src/pages/en/_meta.js
@@ -0,0 +1,11 @@
+export default {
+  index: {},
+  _: {
+    type: "separator",
+    title: "Features",
+  },
+  wiki: {
+    title: "Wiki",
+    route: "/wiki",
+  },
+};
