42e4c39d8a526c0680e69c3e3bdbad556521c422
Jingles <jinglescode@gmail.com>
2025-03-07 10:16:52 +0800

    minor page meta edits

--- apps/docs/src/configs/index.ts
+++ apps/docs/src/configs/index.ts
@@ -1,5 +1,5 @@
 export const ROOT_URL = "https://polkadot.meshjs.dev";
 
-export const APP_NAME = "polkadot.meshjs.dev";
-export const APP_DESC = "polkadot.meshjs.dev";
+export const APP_NAME = "Mesh Polkadot";
+export const APP_DESC = "Mesh Polkadot providers tools and resources for developers to build on Polkadot.";
 export const PROJECT_GITHUB_URL = "https://github.com/MeshJS/polkadot";
