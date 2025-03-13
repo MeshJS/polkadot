4f1da904e47eb6852e374fb9040bcf6ee7bd6ea0
AltiMario <altimario@gmail.com>
2025-03-13 17:14:08 +0000

    structure refactor

--- apps/docs/src/pages/en/wiki/_meta.js
+++ apps/docs/src/pages/en/wiki/_meta.js
@@ -1,15 +1,19 @@
 export default {
   index: {},
   "0_to_get_oriented": {
-    title: "0. To get oriented",
+    title: "To get oriented",
     route: "/0_to_get_oriented",
   },
   "1_theory_and_comparison": {
-    title: "1. Theory and comparison",
+    title: "Theory and comparison",
     route: "/1_theory_and_comparison",
   },
   "2_getting_hands_on": {
-    title: "2. Getting hands on",
+    title: "Getting hands on",
     route: "/2_getting_hands_on",
+  },
+  "3_smart_contracts_lib": {
+    title: "Smart Contracts Lib",
+    route: "/3_smart_contracts_lib",
   },
 };
